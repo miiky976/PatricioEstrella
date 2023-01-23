e0b76d9c7ac9386420afd7c8e8d6a5cf28e884d0
miiky976 <mruiz3445@gmail.com>
2023-01-22 22:21:38 -0600

    Patricio estrella al 99%

---
+++ src/patricio/Patricio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patricio;
+
+/**
+ *
+ * @author miiky
+ */
+public class Patricio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Screen scr = new Screen();
+        scr.setVisible(true);
+    }
+    
+}
